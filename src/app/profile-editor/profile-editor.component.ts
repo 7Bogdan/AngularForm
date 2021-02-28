3347e115bf401068b3c8059a414d246c913cbc6d
7Bogdan <bogdansemenuck@gmail.com>
2021-02-28 22:07:12 +0200

    pages

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -23,6 +23,7 @@ import { EMAILS } from '../emails';
   templateUrl: './profile-editor.component.html',
   styleUrls: ['./profile-editor.component.scss'],
 })
+
 export class ProfileEditorComponent {
   profileForm = this.fb.group({
     firstName: ['', [Validators.required, Validators.minLength(3)]],
